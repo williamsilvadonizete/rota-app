aca3f5f9143cf3db75548e5a6ba604fa2bc0c4ce
willardanuy <willardanuy@gmail.com>
2025-03-07 13:36:54 -0300

    chore: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'screens/onboarding/onboarding_scrreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'The Flutter Way - Foodly UI Kit',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 240, 139, 23),
+            foregroundColor: const Color.fromARGB(255, 32, 31, 31),
+            minimumSize: const Size(double.infinity, 40),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: bodyTextColor),
+          bodySmall: TextStyle(color: bodyTextColor),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          contentPadding: EdgeInsets.all(defaultPadding),
+          hintStyle: TextStyle(color: bodyTextColor),
+        ),
+      ),
+      home: const OnboardingScreen(),
+    );
+  }
+}
